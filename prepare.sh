b416e53e1c8d50f3f130196ba031ee5d1a00f6d6
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2021-07-17 18:16:26 +0100

    Copying option.html to the option location

--- prepare.sh
+++ prepare.sh
@@ -2,5 +2,6 @@ CACHEBUST=`date +%s`
 sed 's/CACHEBUST/'$CACHEBUST'/g' index.html > docs/index.html
 cp map.js docs/map.min.js
 cp map.css docs/map.min.css
+cp option.html docs/option/index.html
 #uglifyjs --rename map.js > docs/map.min.js
 #uglifycss map.css > docs/map.min.css
